cef5e52dfe6f16a6be37771ccecfe0d256e2a2ba
syedkashan-ops <5424500@gmail.com>
2026-08-19 17:45:35 +0500

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -1,5 +1,8 @@
+import os
+
 from waitress import serve
 from web_app import app
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080, threads=4)
+    port = int(os.environ.get("PORT", 8080))
+    serve(app, host="0.0.0.0", port=port, threads=4)
