23f92d6205d9c1278a24fc648803351b651e0eba
syedkashan-ops <5424500@gmail.com>
2026-08-20 20:46:11 +0500

    Delete run_web_server.py

--- run_web_server.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from waitress import serve
-from web_app import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    serve(app, host="0.0.0.0", port=port, threads=4)
